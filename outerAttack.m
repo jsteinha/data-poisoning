c7507ed00d601403a61ea1a91b47a15cd9ae38c9
Jacob Steinhardt <jacob.steinhardt@gmail.com>
2017-05-10 23:46:05 -0700

    upper bound true

--- outerAttack.m
+++ outerAttack.m
@@ -1,17 +1,18 @@
 clear all;
 name = 'enron';
 load(sprintf('%s/%s_data.mat', name, name));
-epsilon = 0.1;
+epsilon = 2.5;
 %opts = sdpsettings('verbose', 2, 'showprogress', 1, 'solver', 'gurobi', 'gurobi.TimeLimit', 3);
 [N_train, N_test, d, mus, probs, r_sphere, r_slab, r_ones] = processDataLight(X_train, y_train, X_test, y_test);
 %epsilon_tape = [0.06 0.08 0.10 0.12 0.15 0.20 0.30 0.50 0.60 0.80];
-eta = 0.05;
-delta = 1.0;
+eta = 0.01;
+delta = 3.0;
 theta = zeros(d,1);
 theta2 = zeros(d,1);
-MAX_ITER = 3310;
+MAX_ITER = round(epsilon * N_train);
 X_pert = zeros(MAX_ITER, d);
 y_pert = zeros(MAX_ITER, 1);
+losses = zeros(MAX_ITER, 4);
 %
 % initial step
 [g_c, L_c] = nabla_Loss(X_train, y_train, theta);
@@ -19,6 +20,7 @@ theta2 = theta2 + g_c .* g_c;
 theta = theta - eta * g_c ./ (delta + sqrt(theta2));
 % main loop
 opts = sdpsettings('verbose', 0, 'showprogress', 0, 'solver', 'gurobi', 'gurobi.TimeLimit', 3);
+lambda = 0.1;
 %%
 for iter = 1:MAX_ITER
     fprintf(1, '====== STARTING ITERATION %d ======\n', iter);
@@ -80,14 +82,15 @@ for iter = 1:MAX_ITER
     end
     X_pert(iter,:) = xr;
  %   toc;
-    fprintf(1, 'loss: %.4f (clean) | %.4f (poisoned) | %.4f (all)\n', L_c, L_p, L_c + epsilon * L_p);
-    g = g_c + epsilon * g_p;
+    fprintf(1, 'loss: %.4f (clean) | %.4f (poisoned) | %.4f (norm_sq) | %.4f (all)\n', L_c, L_p, norm(theta,2)^2, (L_c + epsilon * L_p)/(1+epsilon) + 0.5 * lambda * norm(theta,2)^2);
+    losses(iter, :) = [L_c, L_p, norm(theta,2)^2, (L_c + epsilon * L_p)/(1+epsilon) + 0.5 * lambda * norm(theta,2)^2];
+    g = g_c + epsilon * g_p + lambda * theta;
     eta_t = eta; % / sqrt(1 + 0.1 * iter);
     theta2 = theta2 + g .* g;
     theta = theta - eta_t * g ./ (delta + sqrt(theta2));
 end
 %%
-X_train2 = repmat(X_train, [4 1]);
-y_train2 = repmat(y_train, [4 1]);
-N_tot = 4 * N_train + length(y_pert);
-[loss, acc, theta_pert] = train([X_train2;X_pert], [y_train2;y_pert], 3*eta, delta, N_tot, d, 99999, 5);
+X_train2 = repmat(X_train, [1 1]);
+y_train2 = repmat(y_train, [1 1]);
+N_tot = 1 * N_train + length(y_pert);
+[loss, acc, theta_pert] = train([X_train2;X_pert], [y_train2;y_pert], eta, delta, N_tot, d, 99999, 10, 0.1);
